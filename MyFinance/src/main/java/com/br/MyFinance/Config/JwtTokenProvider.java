e8e20baa4bab07a10b499afc8aca42726f469dad
Win/João Guilherme <joaoguilherme.amaral05@gmail.com>
2025-05-29 00:52:43 -0300

    continuação jwtProvider

--- MyFinance/src/main/java/com/br/MyFinance/Config/JwtTokenProvider.java
+++ MyFinance/src/main/java/com/br/MyFinance/Config/JwtTokenProvider.java
@@ -1,13 +1,17 @@
 package com.br.MyFinance.Config;
 
+import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.br.MyFinance.Dto.Response.TokenResponseDto;
+import com.br.MyFinance.Model.UsuarioPermissaoModel;
 import jakarta.annotation.PostConstruct;
+import jakarta.servlet.Servlet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.util.Base64;
 import java.util.Date;
@@ -44,7 +48,8 @@ public class JwtTokenProvider {
     }
 
     private String getAtualizarToken(String nomeDeUsuario, List<String> permissoes, Date now) {
-        return "";
+        String issueUrl = ServletUriComponentsBuilder.fromCurrentContextPath().build().toUriString();
+        return JWT.create().withClaim("roles", permissoes).toString();
     }
 
     private String getTokenDeAcesso(String nomeDeUsuario, List<String> permissoes, Date now, Date validity) {
